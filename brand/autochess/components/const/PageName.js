10a94aeae45c892eff744323fcaf9f0a149fb10c
silex5z <34494803+silex5z@users.noreply.github.com>
2021-02-25 20:28:29 +0900

    update

--- brand/autochess/components/const/PageName.js
+++
@@ -1,7 +0,0 @@
-export const HOME = 'home'
-export const SYNERGY = 'synergy'
-export const LINE_UP = 'line-up'
-export const OBJECT = 'object'
-export const ITEM = 'item'
-export const RANKING = 'ranking'
-export const ACP = 'acp'
